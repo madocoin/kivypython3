a9685401de395a9069897673d1421a47e1e8e283
Wassim messauod douak <51889120+madocoin@users.noreply.github.com>
2023-08-22 09:31:02 -0700

    project
    kivy projects

--- videoplay.py
+++ videoplay.py
@@ -1,27 +1,89 @@
 #!use\bin\python3
 import kivy
+import random
 import pathlib
 import threading
-#import skip_dirs
+#import skip_dirs 'for skip dirs'
+#from glob import glob
 kivy.require('1.9.0')# app and software
 from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.video import Video
 from os.path import dirname, join
+#from display_dir_tree import tree
 from kivy.core.window import Window
-from kivy.uix.videoplayer import VideoPlayer
+#from unique_path import unique_path
+from kivy.uix.carousel import Carousel
+from kivy.uix.gridlayout import GridLayout
 
-Window.size = (400, 400)
-Window.title = 'VideoPlayer python3'
-Window.clearcolor = (222//255.0, 0,0 ,0)
 '''
-run and play your mp4 video, moves, clip player with python kivy kv
+run and play your mp4 video, moves, clip player with python kivy kv 
 '''
-path = str("vedio.MKV")
 
+#load your string kivy or file kivy with Builder.load_file(file.kv)
+Builder.load_string(r'''
+<gridlayout>:
+    cols: 1 #: desin window
+    Button:
+        text: '<--Back'
+        on_release:
+            root.parent.parent.load_previous()
+    Video:# i have one video you can use for loop for more videos
+        source: 'video.MKV'
+        play: True
+
+    Button:
+        text: 'next-->'
+        on_release:
+            root.parent.parent.load_next()
+''')
+
+n_of_colors = 255.00 # the number of colors in kivy
+#random color rom 00.00 to 255.00
+color = random.randint(0, n_of_colors)
+#create color window
+Window.clearcolor = (color/n_of_colors, 0,0 ,0)
+#change size
+Window.size = (500, 300)
+
+#get direct video
+videodirect = join(dirname(__file__), 'video.MKV')
+
+#for multivideos
+if pathlib.Path(videodirect).exists(): 
+    pass;
+else: 
+    pathin = repr(input('entree your video path here'))
+    if pathlib.Path(pathin).exists():
+        videodirect = pathin
+    else:
+        #you can use raise or assert method
+        BaseException(f'this path--> {pathin} is not exists')
+        print(r'''#entre your path video here for extract all videos from your path
+path = repr(input('input your path videos'))
+if  pathlib.Path(path).exists():
+    pass;
+else: 
+    BaseException(f'this path--> {path} is not exists')
+pathvideos = pathlib.Path(path).rglob("*")
+listvideos = [item for item in pathvideos if item.is_file()]
+listvideos = listvideos.append(videodirect) if videodirect else listvideos
+''')
+        exit(0)
+
+#require gridlayaout for show your buttons and kivy code file
+class gridlayout(GridLayout):
+    pass;
+
+#build app
 class VideoPlay(App):
 
     def build(self):
-    	# path of you vedio mp4 in source
-    	return(VideoPlayer(source=f"{path}", state='stop'))
+        carousel = Carousel()
+        # x is video from listvideo
+        for x in range(4):
+            carousel.add_widget(gridlayout())
+        return carousel;
 
 #run app with multithrading
 if __name__ == '__main__':
